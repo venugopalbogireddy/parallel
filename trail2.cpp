65a33a2fba1d8476995bf9987d3b7db9651f9cbe
Harsh Ranjan <tg855993@c455-041.stampede2.tacc.utexas.edu>
2019-04-20 20:37:17 -0500

    Algorithm working fine, No errors

--- trail2.cpp
+++ trail2.cpp
@@ -7,35 +7,19 @@
 #define _ ios_base::sync_with_stdio(false);cin.tie(0);
 using namespace std;
 
-#define pb push_back
-#define pob pop_back
-#define pf push_front
-#define pof pop_front
-#define mp make_pair
-#define all(a) a.begin(),a.end()
-#define bitcnt(x) __builtin_popcountll(x)
-#define MOD 1000000007
-#define PI 3.14159265
-#define BLOCK 20000
-#define MAXN 500005
-#define N (1<<28)
-
-typedef unsigned long long int uint64;
-typedef long long int int64;
-
-int a[N];
-
 void par_prefix_sum(int* b, int n) {
 	for (int i=1; i<n; i++) {
 		b[i] = b[i]+b[i-1];
 	}
 }
 
-int par_partition(int q, int r, int x) {
+int par_partition(int* a, int q, int r, int x) {
 	int n = r-q+1;
 	if (n==1)
 		return q;
-	int b[n], lt[n], gt[n];
+	int *b = new int[n];
+	int *lt = new int[n];
+	int *gt = new int[n];
 	cilk_for(int i=0; i<n; i++) {
 		b[i] = a[q+i];
 		if(b[i]<x) lt[i]=1;
@@ -54,62 +38,21 @@ int par_partition(int q, int r, int x) {
 			a[k+gt[i]] = b[i];
 	}
 	return k;
-}
-
-int partition (int low, int high) 
-{ 
-    int pivot = a[high];    // pivot 
-    int i = (low - 1);  // Index of smaller element 
-  
-    for (int j = low; j <= high- 1; j++) 
-    { 
-        // If current element is smaller than or 
-        // equal to pivot 
-        if (a[j] <= pivot) 
-        { 
-            i++;    // increment index of smaller element 
-            int t = a[i];
-	    a[i] = a[j];
-	    a[j] = t; 
-        } 
-    }
-    int t=a[i+1];
-    a[i+1]=a[high];
-    a[high]=t; 
-    return (i + 1); 
-}
-/* The main function that implements QuickSort 
- arr[] --> Array to be sorted, 
-  low  --> Starting index, 
-  high  --> Ending index */
-void quickSort(int low, int high) 
-{ 
-    if (low < high) 
-    { 
-        /* pi is partitioning index, arr[p] is now 
-           at right place */
-        int pi = partition(low, high); 
-  
-        // Separately sort elements before 
-        // partition and after partition 
-        quickSort(low, pi - 1); 
-        quickSort(pi + 1, high); 
-    } 
 } 
 
-void par_randomized_quicksort(int q, int r) {
+void par_randomized_quicksort(int* a, int q, int r) {
 	int n=r-q+1;
-	cout<<"Quicksort "<<q<<" "<<r<<" "<<n<<endl;
+	//cout<<"Quicksort "<<q<<" "<<r<<" "<<n<<endl;
 	if (n<=32)
-		quickSort(q, r);
+		sort(a+q, a+r+1);
 	else {
 		// select a random number
-		int x = rand() % n + q;
-		cout<<"Random Index "<<x<<"~"<<a[x]<<endl;
-		int k = par_partition(q, r, a[x]);
-		cout<<"Random Index position "<<x<<"~"<<k<<endl;
-		cilk_spawn par_randomized_quicksort(q, k-1);
-		par_randomized_quicksort(k+1, r);
+		int x = (rand() % n) + q;
+		//cout<<"Random Index "<<x<<"~"<<a[x]<<endl;
+		int k = par_partition(a, q, r, a[x]);
+		//cout<<"Random Index position "<<x<<"~"<<k<<endl;
+		cilk_spawn par_randomized_quicksort(a, q, k-1);
+		par_randomized_quicksort(a, k+1, r);
 		cilk_sync;
 	}
 }
@@ -117,15 +60,16 @@ void par_randomized_quicksort(int q, int r) {
 int main(int argc, char* argv[]) {
 	if (argc != 2)
 	{
-		cout<<"Usage: /quicksort-par N"<<endl;
+		cout<<"Usage: /quicksort-par input-array-size #workers"<<endl;
 	}
-        __cilkrts_set_param("nworkers",argv[2]);
+        //__cilkrts_set_param("nworkers",argv[2]);
 	// initializing random numbers to the array
 	int n = atoi(argv[1]);
-        cout<<"Array size "<<N<<" input "<<n<<endl;
+	int *a = new int[n];
+        cout<<"Array size "<<"input "<<n<<endl;
 	for (int i = 0; i < n; ++i)
 	{
-		a[i] = rand() % 5000000;
+		a[i] = n-1-i;
 	}
 	cout<<"Array created"<<endl;
 	/*for (int i=0; i<n; i++)
@@ -138,9 +82,16 @@ int main(int argc, char* argv[]) {
     //calling function
     //
     cout<<"Calling Quicksort"<<endl;
-    par_randomized_quicksort(0, n-1);
+    par_randomized_quicksort(a, 0, n-1);
     time_t end = time(NULL);
     double time_taken = end-start;
+
+	for(int i=0; i<n; i++) {
+		if(a[i]!=i) {
+			cout<<"NO-SORT "<<i<<endl;
+			break;
+		}
+	}
 
     printf("Time Taken for Base size %d is %.6lf\n",n,time_taken);
 	/*for (int i=0; i<n; i++)
